91067a93565a54d22ad2a3fdfcd5ba51a824aac1
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-01-25 17:21:27 +0500

    Refactoring constant

--- src/main/java/uz/paynet/constants/ServiceSettingsConstants.java
+++ src/main/java/uz/paynet/constants/ServiceSettingsConstants.java
@@ -2,5 +2,7 @@ package uz.paynet.constants;
 
 public class ServiceSettingsConstants {
 
-    public static final String SERVICE_SETTINGS = "/service_settings";
+    public static final String SERVICE_SETTINGS_URL = "/service_settings";
+    public static final String AGENT_ID_PARAM = "/{agentId}";
+    public static final String SERVICE_ID_PARAM = "/{serviceId}";
 }
