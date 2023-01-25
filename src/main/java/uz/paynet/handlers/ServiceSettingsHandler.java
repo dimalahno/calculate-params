f06df59e042f2da0f42aae473f6ded54d68c1c22
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-01-25 17:22:37 +0500

    Add routes and handlers for getting service settings by agent id or/and service id.

--- src/main/java/uz/paynet/handlers/ServiceSettingsHandler.java
+++ src/main/java/uz/paynet/handlers/ServiceSettingsHandler.java
@@ -1,7 +1,6 @@
 package uz.paynet.handlers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -11,26 +10,54 @@ import uz.paynet.repository.ServiceSettingsRepository;
 
 import java.net.URI;
 
-import static uz.paynet.constants.ServiceSettingsConstants.SERVICE_SETTINGS;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static uz.paynet.constants.ServiceSettingsConstants.SERVICE_SETTINGS_URL;
 
 @Component
 @AllArgsConstructor
 public class ServiceSettingsHandler {
 
-    static Mono<ServerResponse> notFound = ServerResponse.notFound().build();
+    private final static String AGENT_ID = "agentId";
+    private final static String SERVICE_ID = "serviceId";
+    private final static Mono<ServerResponse> NOT_FOUND = ServerResponse.notFound().build();
 
     final ServiceSettingsRepository repository;
 
     public Mono<ServerResponse> getAllServiceSettings(ServerRequest request) {
         return ServerResponse.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(repository.findAll(), ServiceSettings.class);
+                .contentType(APPLICATION_JSON)
+                .body(repository.findAll(), ServiceSettings.class)
+                .switchIfEmpty(NOT_FOUND);
     }
 
     public Mono<ServerResponse> createServiceSettings(ServerRequest request) {
         Mono<ServiceSettings> newSettings = request.bodyToMono(ServiceSettings.class);
-        return newSettings.flatMap(settings -> ServerResponse.created(URI.create(SERVICE_SETTINGS))
-                .contentType(MediaType.APPLICATION_JSON)
+        return newSettings.flatMap(settings -> ServerResponse.created(URI.create(SERVICE_SETTINGS_URL))
+                .contentType(APPLICATION_JSON)
                 .body(repository.save(settings), ServiceSettings.class));
+    }
+
+    public Mono<ServerResponse> getServiceSettingsByAgentId(ServerRequest request) {
+        var agentId = Integer.valueOf(request.pathVariable(AGENT_ID));
+        return ServerResponse.ok()
+                .contentType(APPLICATION_JSON)
+                .body(repository.findByAgentId(agentId), ServiceSettings.class)
+                .switchIfEmpty(NOT_FOUND);
+    }
+
+    public Mono<ServerResponse> getServiceSettingsByServiceId(ServerRequest request) {
+        var serviceId = Integer.valueOf(request.pathVariable(SERVICE_ID));
+        return ServerResponse.ok()
+                .contentType(APPLICATION_JSON)
+                .body(repository.findByServiceId(serviceId), ServiceSettings.class)
+                .switchIfEmpty(NOT_FOUND);
+    }
+
+    public Mono<ServerResponse> getServiceSettingsByAgentIdAnServiceId(ServerRequest request) {
+        var agentId = Integer.valueOf(request.pathVariable(AGENT_ID));
+        var serviceId = Integer.valueOf(request.pathVariable(SERVICE_ID));
+        return ServerResponse.ok()
+                .contentType(APPLICATION_JSON)
+                .body(repository.findByAgentIdAndServiceId(agentId, serviceId), ServiceSettings.class);
     }
 }
--- src/main/java/uz/paynet/routers/ServiceSettingsRouter.java
+++ src/main/java/uz/paynet/routers/ServiceSettingsRouter.java
@@ -9,16 +9,22 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import uz.paynet.handlers.ServiceSettingsHandler;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
-import static uz.paynet.constants.ServiceSettingsConstants.SERVICE_SETTINGS;
+import static uz.paynet.constants.ServiceSettingsConstants.*;
 
 @Configuration
 public class ServiceSettingsRouter {
 
     @Bean
     public RouterFunction<ServerResponse> serviceSettingsRoute(ServiceSettingsHandler handler) {
-        return RouterFunctions.route(GET(SERVICE_SETTINGS).and(accept(MediaType.APPLICATION_JSON)),
+        return RouterFunctions.route(GET(SERVICE_SETTINGS_URL).and(accept(MediaType.APPLICATION_JSON)),
                         handler::getAllServiceSettings)
-                .andRoute(POST(SERVICE_SETTINGS).and(accept(MediaType.APPLICATION_JSON)),
+                .andRoute(GET(SERVICE_SETTINGS_URL.concat(AGENT_ID_PARAM)).and(accept(MediaType.APPLICATION_JSON)),
+                        handler::getServiceSettingsByAgentId)
+                .andRoute(GET(SERVICE_SETTINGS_URL.concat(SERVICE_ID_PARAM)).and(accept(MediaType.APPLICATION_JSON)),
+                        handler::getServiceSettingsByServiceId)
+                .andRoute(GET(SERVICE_SETTINGS_URL.concat(AGENT_ID_PARAM).concat(SERVICE_ID_PARAM)).and(accept(MediaType.APPLICATION_JSON)),
+                        handler::getServiceSettingsByAgentIdAnServiceId)
+                .andRoute(POST(SERVICE_SETTINGS_URL).and(accept(MediaType.APPLICATION_JSON)),
                         handler::createServiceSettings);
     }
 }
