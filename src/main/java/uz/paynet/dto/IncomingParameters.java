da3d71dd9fecfbb66942ed8a402404c2a6fcfa79
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-01-26 17:25:31 +0500

    refactoring type of field

--- src/main/java/uz/paynet/dto/IncomingParameters.java
+++ src/main/java/uz/paynet/dto/IncomingParameters.java
@@ -1,14 +1,19 @@
 package uz.paynet.dto;
 
 import lombok.Data;
+import lombok.NonNull;
 import uz.paynet.documents.CardType;
 
 @Data
 public class IncomingParameters {
 
+    @NonNull
     private Long amount;
+    @NonNull
     private Long purchasedAmount;
-    private String agentId;
-    private String serviceId;
+    @NonNull
+    private Integer agentId;
+    @NonNull
+    private Integer serviceId;
     private CardType cardType;
 }
