51590d517f499c83e9b8021f791ef3049e3b68cf
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-01-27 11:29:22 +0500

    remove unnecessary methods

--- src/main/java/uz/paynet/repository/ServiceSettingsRepository.java
+++ src/main/java/uz/paynet/repository/ServiceSettingsRepository.java
@@ -3,11 +3,7 @@ package uz.paynet.repository;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import uz.paynet.documents.CardType;
 import uz.paynet.documents.ServiceSettings;
-
-import java.util.Date;
 
 @Repository
 public interface ServiceSettingsRepository extends ReactiveMongoRepository<ServiceSettings, String> {
@@ -18,11 +14,5 @@ public interface ServiceSettingsRepository extends ReactiveMongoRepository<Servi
 
     Flux<ServiceSettings> findByAgentIdAndServiceId(Integer agentId, Integer serviceId);
 
-    Mono<ServiceSettings> findByAgentIdAndServiceIdAndStatus(Integer agentId, Integer serviceId, Integer status);
-
-    Mono<ServiceSettings> findByAgentIdAndServiceIdAndCardTypeAndStatus(Integer agentId, Integer serviceId,
-                                                                        CardType cardType, Integer status);
-
-    Mono<ServiceSettings> findByAgentIdAndServiceIdAndStatusAndStartDateAfter(Integer agentId, Integer serviceId,
-                                                                              Integer status, Date startDate);
+    Flux<ServiceSettings> findByAgentIdAndServiceIdAndStatus(Integer agentId, Integer serviceId, Integer status);
 }
